dc10cf9c1b958543ab0ad1f8edd1dcc4b863ed03
ciyer87 <chitra.iyer00@gmail.com>
2021-07-15 17:39:00 -0700

    fixed bug in thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,17 +1,39 @@
 const router = require('express').Router();
-const { addThought, removeThought, addReaction, removeReaction} = require('../../controllers/thought-controller');
+const { 
+    addThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThought, 
+    removeThought, 
+    addReaction, 
+    removeReaction
+} = require('../../controllers/thought-controller');
 
+router
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought);
+
+//    /api/thoughts/<userId>
 router
     .route('/:userId')
     .post(addThought);
+
 router
     .route('/:userId/:thoughtId')
     .delete(removeThought);
+
+// /api/thoughts/<userId>/<thoughtID>    
 router
-    .route('/:thoughtID/reactions')
-    .post(addReaction)
+    .route('/:userId/:thoughtId')
+    .post(addReaction);
+
 router
-    .route('/:thoughtId/reactions/:reactionId')
+    .route('/:userID/:thoughtID/:reactionId')
     .delete(removeReaction);
 
 module.exports = router;
